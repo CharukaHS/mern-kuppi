1cfe1b0c9b49b0f2ed7806b204f0f8fe7ccd6259
CharukaHS <charuka@protonmail.com>
2021-09-14 21:36:58 +0530

    Made Todo panel reactive

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,25 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import type { NextPage } from "next";
-import { Box, Container, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Container, Flex, Heading } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import TodoCard from "../components/TodoCard";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Textarea,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
-const dashboard: NextPage = () => {
+type Inputs = {
+  newtodo: string;
+};
+
+const Dashboard: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [todos, settodos] = useState<string[]>([]);
+
+  const onSubmit = (value: Inputs) => {
+    settodos([...todos, value.newtodo]);
+    onClose();
+    reset();
+  };
+
   return (
     <Flex direction="column" justify="center" align="center" height="100vh">
       <Heading>Todo App 👋</Heading>
 
       <Container mt="35px" overflowY="auto" maxH="75%">
-        <TodoCard task="Bring Cheese" />
-        <TodoCard task="Bring Koththu" />
-        <TodoCard task="Bring Cheese" />
-        <TodoCard task="Bring Cheese" />
-        <TodoCard task="Bring Cheese" />
+        {todos.map((todo) => {
+          return <TodoCard key={todo} task={todo} />;
+        })}
       </Container>
 
-      <Button mt={4}>Add New</Button>
+      <Button mt={4} onClick={onOpen}>
+        Add New
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <ModalHeader>New Todo</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Textarea
+                placeholder="Here is a sample placeholder"
+                {...register("newtodo", { required: true })}
+              />
+              {errors.newtodo && <span>This field is required</span>}
+            </ModalBody>
+
+            <ModalFooter>
+              <Button mr={3} onClick={onClose}>
+                Reset
+              </Button>
+              <Button type="submit" colorScheme="teal">
+                Save
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
 
-export default dashboard;
+export default Dashboard;
