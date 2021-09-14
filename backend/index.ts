30e9caeee41645b396b0e18b89b5cde0983a055d
CharukaHS <charuka@protonmail.com>
2021-09-14 22:09:46 +0530

    Express js init

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello again");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from /");
+});
+
+app.listen(3001, () => {
+  console.log("Backend is running");
+});
