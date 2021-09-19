d99017d1aafd9eee61c68c71d538272001ee6317
CharukaHS <charuka@protonmail.com>
2021-09-19 20:59:37 +0530

    Connect to mongodb

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -6,6 +7,16 @@ app.get("/", (req, res) => {
   res.send("Hello from /");
 });
 
-app.listen(3001, () => {
-  console.log("Backend is running");
+const PORT = 3001;
+app.listen(PORT, async () => {
+  console.log("App is listening on port", PORT);
+  try {
+    await mongoose.connect("mongodb://localhost:27017/todos", {
+      auth: { username: "root", password: "example" },
+      authSource: "admin",
+    });
+    console.log("Connected to mongo db");
+  } catch (error) {
+    console.error("Error connecting to mongodb", error);
+  }
 });
